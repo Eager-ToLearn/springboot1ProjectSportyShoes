3896e2d5ffb8841e170fdcaf46b852fb58262172
Eager-ToLearn <oarabilebooo@gmail.com>
2024-01-30 11:57:38 +0200

    for jenking

---
+++ src/main/java/pac/springboot/Springboot1ProjectSportyShoesApplication.java
@@ -0,0 +1,13 @@
+package pac.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1ProjectSportyShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot1ProjectSportyShoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pac/springboot/Springboot1ProjectSportyShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package pac.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot1ProjectSportyShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
